fd345a889e8fc958628e254d68dc190bbc001681
Marcos Bonifasi <marcosbonifasi19@gmail.com>
2022-08-09 18:52:04 -0600

    basic math operators

--- src/main.go
+++ src/main.go
@@ -12,9 +12,9 @@ func main() {
 	fmt.Println("pi2", pi2)
 
 	// Declaring variables
-	base := 12
-	var altura int = 14
-	var area int
+	// base := 12
+	// var altura int = 14
+	// var area int
 
 	// Zero values
 	var a int
@@ -23,11 +23,65 @@ func main() {
 	var d bool
 
 	fmt.Println(a, b, c, d)
-	fmt.Println(base, altura, area)
+	// fmt.Println(base, altura, area)
 
 	// Square area
 	const squareBase = 10
 	squareArea := squareBase * squareBase
 
 	fmt.Println(squareArea)
+	// ------
+
+	x := 10
+	y := 50
+
+	result := x + y
+	fmt.Println("Suma:", result)
+
+	result = y - x
+	fmt.Println("Resta: ", result)
+
+	result = x * y
+	fmt.Println("Multiplicacion: ", result)
+
+	result = y / x
+	fmt.Println("Division: ", result)
+
+	// modulo
+	result = y % x
+	fmt.Println("Modulo:", result)
+
+	// Incrementar
+	x++
+	fmt.Println("Incremental: ", x)
+
+	// Decremental
+	x--
+	fmt.Println("Decremental: ", x)
+
+	// Retos
+	// - area cuadrado
+	// - area trapecio
+	// - area circulo
+
+	// Area cuadrado
+	var width float64 = 10
+	var heigth float64 = 20
+	var area float64
+
+	area = heigth * width
+	fmt.Println("Area de rectangulo: ", area)
+
+	// Area trapecio
+	var mayorBase float64
+	var minorBase float64
+
+	var trapecioArea = ((mayorBase + minorBase) / 2) * heigth
+	fmt.Println("Area de trapecio: ", trapecioArea)
+
+	// Area circulo
+	var radio float64 = 2.2
+	var areaCircle float64 = pi * (radio * radio)
+
+	fmt.Println("Area de circulo: ", areaCircle)
 }
